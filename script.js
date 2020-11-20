30e2871b4a21fc5aed2db224042e3c821bc34ea1
TalyssonG <lopestalysson99@gmail.com>
2020-11-20 14:19:39 -0300

    Atualização

--- script.js
+++ script.js
@@ -12,17 +12,5 @@ document.querySelector("#prazo").addEventListener("change", function () {
     atualizarPreco()
 })
 
-function atualizarPreco(){
-    const qtde = document.querySelector("#qtde").value
-    const temJS = document.querySelector("#js").checked
-    const incluiLayout = document.querySelector("#layout-sim").checked
-    const prazo = document.querySelector("#prazo").value
-  
-    let preco = qtde * 100;
-    if (temJS) preco *= 1.1
-    if (incluiLayout) preco += 500
-    let taxaUrgencia = 1 - prazo*0.1;
-    preco *= 1 + taxaUrgencia
-   
+
     document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
-}
